f2d2e07259c8f69bf7ffdc796387a0b5c2cf3936
GusVenegasS <jaime.venegas@epn.edu.ec>
2023-12-12 08:53:50 -0500

    clase 11

---
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/ACicloVida.kt
@@ -0,0 +1,11 @@
+package com.example.b2023gr2sw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ACicloVida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+    }
+}
